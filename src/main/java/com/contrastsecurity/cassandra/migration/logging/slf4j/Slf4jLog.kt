7f73ea61057bdda32f50b326ce43cabda8a80c0c
Herdy Handoko <herdy.handoko@gmail.com>
2016-08-24 16:42:45 +0800

    Converted Slf4j logging wrapper to Kotlin code

---
+++ src/main/java/com/contrastsecurity/cassandra/migration/logging/slf4j/Slf4jLog.kt
@@ -0,0 +1,51 @@
+/**
+ * File     : Slf4jLog.kt
+ * License  :
+ *   Original   - Copyright (c) 2010 - 2016 Boxfuse GmbH
+ *   Derivative - Copyright (c) 2016 Citadel Technology Solutions Pte Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.contrastsecurity.cassandra.migration.logging.slf4j
+
+import com.contrastsecurity.cassandra.migration.internal.util.logging.Log
+import org.slf4j.Logger
+
+/**
+ * Wrapper for a Slf4j logger.
+ *
+ * @param logger The original Slf4j Logger.
+ */
+class Slf4jLog(private val logger: Logger) : Log {
+
+    override fun debug(message: String) {
+        logger.debug(message)
+    }
+
+    override fun info(message: String) {
+        logger.info(message)
+    }
+
+    override fun warn(message: String) {
+        logger.warn(message)
+    }
+
+    override fun error(message: String) {
+        logger.error(message)
+    }
+
+    override fun error(message: String, e: Exception) {
+        logger.error(message, e)
+    }
+
+}

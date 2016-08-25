e17843992942657f792eb672eb786d19991aae50
Herdy Handoko <herdy.handoko@gmail.com>
2016-08-25 10:42:49 +0800

    Converted Java migration resolvers to Kotlin code

---
+++ src/main/java/com/contrastsecurity/cassandra/migration/resolver/java/JavaMigrationResolver.kt
@@ -0,0 +1,120 @@
+/**
+ * File     : JavaMigrationResolver.kt
+ * License  :
+ *   Original   - Copyright (c) 2015 - 2016 Contrast Security
+ *   Derivative - Copyright (c) 2016 Citadel Technology Solutions Pte Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.contrastsecurity.cassandra.migration.resolver.java
+
+import com.contrastsecurity.cassandra.migration.api.CassandraMigrationException
+import com.contrastsecurity.cassandra.migration.api.MigrationType
+import com.contrastsecurity.cassandra.migration.api.MigrationVersion
+import com.contrastsecurity.cassandra.migration.api.migration.MigrationChecksumProvider
+import com.contrastsecurity.cassandra.migration.api.migration.MigrationInfoProvider
+import com.contrastsecurity.cassandra.migration.api.migration.java.JavaMigration
+import com.contrastsecurity.cassandra.migration.api.resolver.MigrationResolver
+import com.contrastsecurity.cassandra.migration.info.ResolvedMigration
+import com.contrastsecurity.cassandra.migration.internal.resolver.MigrationInfoHelper
+import com.contrastsecurity.cassandra.migration.internal.resolver.ResolvedMigrationComparator
+import com.contrastsecurity.cassandra.migration.internal.util.ScriptsLocation
+import com.contrastsecurity.cassandra.migration.utils.ClassUtils
+import com.contrastsecurity.cassandra.migration.utils.StringUtils
+import com.contrastsecurity.cassandra.migration.utils.scanner.Scanner
+import java.util.*
+
+/**
+ * Migration resolver for Java migrations.
+ * The classes must have a name like V1 or V1_1_3 or V1__Description or V1_1_3__Description.
+ *
+ * @param classLoader The ClassLoader for loading migrations on the classpath.
+ * @param location The base package on the classpath where to migrations are located.
+ */
+class JavaMigrationResolver(
+    private val classLoader: ClassLoader,
+    private val location: ScriptsLocation?
+) : MigrationResolver {
+
+    /**
+     * Resolves the available migrations.
+     *
+     * @return The available migrations.
+     * @throws CassandraMigrationException when unable to resolve JavaMigrator with the given location.
+     */
+    @Throws(CassandraMigrationException::class)
+    override fun resolveMigrations(): List<ResolvedMigration> {
+        // Guard: If location  is not defined and not a classpath,
+        //        then return an empty JavaMigrations
+        if (location != null && !location.isClassPath) {
+            return ArrayList()
+        }
+
+        try {
+            val classes = Scanner(classLoader).scanForClasses(location, JavaMigration::class.java)
+
+            return classes.map { clazz ->
+                val javaMigration = ClassUtils.instantiate<JavaMigration>(clazz.name, classLoader)
+                val physicalLocation = ClassUtils.getLocationOnDisk(clazz)
+                val executor = JavaMigrationExecutor(javaMigration)
+
+                val migrationInfo = extractMigrationInfo(javaMigration)
+                migrationInfo.physicalLocation = physicalLocation
+                migrationInfo.executor = executor
+                migrationInfo
+            }.sortedWith(ResolvedMigrationComparator())
+        } catch (e: Exception) {
+            throw CassandraMigrationException("Unable to resolve Java migrations in location: " + location, e)
+        }
+    }
+
+    /**
+     * Extracts the migration info from this migration.
+     *
+     * @param javaMigration The migration to analyse.
+     * @return The migration info.
+     * @throws CassandraMigrationException when JavaMigration is missing its description.
+     */
+    @Throws(CassandraMigrationException::class)
+    fun extractMigrationInfo(javaMigration: JavaMigration): ResolvedMigration {
+        var checksum: Int? = null
+        if (javaMigration is MigrationChecksumProvider) {
+            checksum = javaMigration.checksum
+        }
+
+        val version: MigrationVersion
+        val description: String
+        if (javaMigration is MigrationInfoProvider) {
+            version = javaMigration.version
+            description = javaMigration.description
+            if (!StringUtils.hasText(description)) {
+                throw CassandraMigrationException("Missing description for migration " + version)
+            }
+        } else {
+            val className = ClassUtils.getShortName(javaMigration.javaClass)
+            val info = MigrationInfoHelper.extractVersionAndDescription(className, "V", "__", "")
+            version = info.left
+            description = info.right
+        }
+
+        val script = javaMigration.javaClass.name
+
+        val resolvedMigration = ResolvedMigration()
+        resolvedMigration.version = version
+        resolvedMigration.description = description
+        resolvedMigration.script = script
+        resolvedMigration.checksum = checksum
+        resolvedMigration.type = MigrationType.JAVA_DRIVER
+        return resolvedMigration
+    }
+}

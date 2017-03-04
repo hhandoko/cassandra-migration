8646a15802b4a6d937e5e7ce727433f6f5b4ad1f
Herdy Handoko <herdy.handoko@gmail.com>
2017-03-04 08:31:50 +0800

    Reenabled disable embedded Cassandra flag for Travis CI script

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -18,5 +18,5 @@
 #   limitations under the License.
 ###
 
-mvn verify -Dit.config.file=src/test/resources/application.it-test.conf
+mvn verify -Dit.config.file=src/test/resources/application.it-test.conf -Dcassandra.migration.disable_embedded=true
 
